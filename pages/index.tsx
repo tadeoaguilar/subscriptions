660a6fc1900a142a6e41501d5083e764bfeca6a2
Tadeo Aguilar <Tadeo.Aguilar@FINNING.COM>
2022-10-13 18:02:37 -0400

    Hero page 1200 800

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,76 +7,31 @@ import HeroText from '../ui/components/HeroText'
 import { motion } from "framer-motion";
 import { title } from 'process'
 const Home: NextPage = () => {
-   const titleAnim = {
-    hidden: { y: 200 },
-    show: {
-      y: 0,
-      transition: { duration: .75, ease: "easeOut" },
-    },
-  };
-  const fade = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: { ease: "easeOut", duration: 3 },
-    },
-  };
-
-
-   const btnLtR = {
-    hidden: { x: 60 },
-    show: {
-      x: 0 ,
-      transition: { duration: .75, ease: "easeOut" },
-    },
-  };
 
 
   return (
-     <div className={styles.frame}>
-    <div className={styles.container}>
+
+    <div className={styles.frame}>
      
       <div className={styles.container}>
         <Image src={"/lamp_1200_800.png" } alt="Test" width={1200} height={800} className={styles.landingImage} />
-        <div className={styles.navbar}>
+        <nav className={styles.navbar}>
           <div className={styles.logo}>
-          <Image src={"/idea.png" } alt="Test" width={68} height={59}  />
-            INSIGHT
+          <Image src={"/idea.png" } alt="Test" width={31} height={41}  />
+           <span className={styles.logoTxt}> INSIGHT</span>
           </div>
           <MenuBar title='Str'   /> 
-        </div>
-        <motion.div className={styles.hero1} 
-                  variants={titleAnim}
-                   animate="show"
-                   initial= "hidden"
-                   
-                   
-                   >
-            <p > IDEAS TO LIGHT YOUR HOME AND SPIRIT </p>
-        </motion.div>
-        <motion.div className={styles.hero2} 
-                    variants= {fade}
-                    animate="show"
-                    initial="hidden"
-        >
-            <p > Learn from <span className={styles.accent}>+100,000</span> experiences from all around the world.</p>
-
-        </motion.div>
-        <motion.div  
-                className={styles.button} 
-                variants= {btnLtR}
-                animate="show"
-                initial="hidden"
-        >
-          Subscribe
-        </motion.div>
+        </nav>
+        <HeroText title='Str' />
       </div>
-      
+      <div className={styles.container}>
+      casa
+      </div>
      
     </div>
     
+
     
-    </div>
     
   )
 }
--- ui/components/HeroText/HeroText.tsx
+++ ui/components/HeroText/HeroText.tsx
@@ -1,20 +1,62 @@
 import React,{FC} from 'react'
-
+import { motion } from "framer-motion";
 interface MenuProps {
     title: string;
 }
 import styles from './HeroBar.module.css'
 
 const HeroText:FC<MenuProps>  = ({title }) => {
-  
+  const titleAnim = {
+    hidden: { y: 200 },
+    show: {
+      y: 0,
+      transition: { duration: 2, ease: "easeOut" },
+    },
+  };
+  const fade = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: { ease: "easeOut", duration: 2 },
+    },
+  };
+
+
+   const btnLtR = {
+    hidden: { x: 60 },
+    show: {
+      x: 0 ,
+      transition: { duration: 2, ease: "easeOut" },
+    },
+  };
   return (
     <>
-        <div className={styles.hero1} >
-            IDEAS TO LIGHT YOUR HOME AND SPIRIT
-        </div>
-        <div className={styles.hero2} >
-            Lear from <span> +100,000</span> experiences from around the world.
+       <div className = {styles.heroText}>
+          <motion.div className={styles.hero1} 
+                    variants={titleAnim}
+                    animate="show"
+                    initial= "hidden"
+                    
+                    
+                    >
+              <p className={styles.line}> IDEAS TO LIGHT YOUR HOME AND SPIRIT </p>
+          </motion.div>
+          <motion.div className={styles.hero2} 
+                      variants= {fade}
+                      animate="show"
+                      initial="hidden"
+          >
+              <p className={styles.line} > Learn from <span className={styles.accent}>+100,000</span> experiences from all around the world.</p>
 
+          </motion.div>
+          <motion.div  
+                  className={styles.button} 
+                  variants= {btnLtR}
+                  animate="show"
+                  initial="hidden"
+          >
+            Subscribe
+          </motion.div>
         </div>
     </>
   )
